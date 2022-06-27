1c764a209d983e3b8fa9995acd6797d34a99cf2d
Adeonir Kohl <adeonir@gmail.com>
2022-06-27 18:09:23 -0300

    Add new task form

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,16 @@
 import styles from './Main.module.scss'
+import { Button } from '~/components/Button'
+import { Input } from '~/components/Input'
 
 export function Main() {
-  return <main className={styles.main}>Main</main>
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <form className={styles.form}>
+          <Input placeholder="Adicione uma nova tarefa" />
+          <Button />
+        </form>
+      </div>
+    </main>
+  )
 }
