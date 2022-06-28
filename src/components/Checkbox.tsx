b9c052bf7d5dc6f75d2c80ddd25a7f7e4cc2b71e
Adeonir Kohl <adeonir@gmail.com>
2022-06-28 09:04:15 -0300

    Update checkbox component

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,12 +2,21 @@ import { InputHTMLAttributes } from 'react'
 
 import styles from './Checkbox.module.scss'
 
-type Props = InputHTMLAttributes<HTMLInputElement>
+type Props = {
+  checked?: boolean
+  onChange?: (status: boolean) => void
+} & InputHTMLAttributes<HTMLInputElement>
 
-export function Checkbox(props: Props) {
+export function Checkbox({ checked, onChange, ...props }: Props) {
   return (
     <div className={styles.wrapper}>
-      <input className={styles.checkbox} type="checkbox" {...props} />
+      <input
+        className={styles.checkbox}
+        type="checkbox"
+        checked={checked}
+        onChange={onChange}
+        {...props}
+      />
     </div>
   )
 }
