71e1d31109bbdee879a56560ecb36db9317972d3
Adeonir Kohl <adeonir@gmail.com>
2022-06-28 22:34:40 -0300

    Add logic to task list

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,20 +1,60 @@
+import { useEffect, useMemo, useState } from 'react'
+
 import { EmptyState } from '~/components/EmptyState'
+import { ListItem } from '~/components/ListItem'
+import type { ItemProps } from '~/components/ListItem'
 
 import styles from './TaskList.module.scss'
 
-export function TaskList() {
+type Props = {
+  items: ItemProps[]
+  onComplete: (id: string) => void
+  onDelete: (id: string) => void
+}
+
+export function TaskList({ items, onComplete, onDelete }: Props) {
+  const [list, setList] = useState(items)
+
+  const isEmpty = useMemo(() => list.length === 0, [list])
+
+  const created = useMemo(() => list.length, [list])
+
+  const completed = useMemo(() => {
+    const completed = list.filter(({ isCompleted }) => isCompleted).length
+    return created === 0 ? completed : `${completed} de ${created}`
+  }, [created, list])
+
+  useEffect(() => {
+    setList(
+      items.sort((a, b) => (a.isCompleted ? 1 : 0) - (b.isCompleted ? 1 : 0))
+    )
+  }, [items])
+
   return (
     <section className={styles.list}>
       <header>
         <div className={styles.created}>
-          Tarefas criadas <span className={styles.counter}>0</span>
+          Tarefas criadas <span className={styles.counter}>{created}</span>
         </div>
         <div className={styles.completed}>
-          Concluídas <span className={styles.counter}>0</span>
+          Concluídas <span className={styles.counter}>{completed}</span>
         </div>
       </header>
       <main>
-        <EmptyState />
+        {isEmpty ? (
+          <EmptyState />
+        ) : (
+          <>
+            {list.map((item) => (
+              <ListItem
+                key={item.id}
+                item={item}
+                onCheck={onComplete}
+                onDelete={onDelete}
+              />
+            ))}
+          </>
+        )}
       </main>
     </section>
   )
