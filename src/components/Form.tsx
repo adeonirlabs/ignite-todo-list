142f9fb4116a7dacc0a1ffa5e744a8c8c3527917
Adeonir Kohl <adeonir@gmail.com>
2022-06-29 00:01:50 -0300

    Add form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,37 @@
+import { nanoid } from 'nanoid'
+import { FormEvent, useState } from 'react'
+
+import { Button } from '~/components/Button'
+import { Input } from '~/components/Input'
+import type { ItemProps } from '~/components/ListItem'
+
+import styles from './Form.module.scss'
+
+type Props = {
+  onChange: (item: ItemProps) => void
+}
+
+export function Form({ onChange }: Props) {
+  const [text, setText] = useState('')
+
+  function handleChange(event: FormEvent<HTMLInputElement>) {
+    setText(event.currentTarget.value)
+  }
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    onChange({ id: nanoid(), title: text.trim(), isCompleted: false })
+    setText('')
+  }
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <Input
+        placeholder="Adicione uma nova tarefa"
+        value={text}
+        onChange={handleChange}
+      />
+      <Button disabled={!(text.length >= 4)} />
+    </form>
+  )
+}
